c5ec7f9c692190ade3d5868f6e87c739d0c6d3b3
Saichandu090 <marrisaichandu143@gmail.com>
2024-12-30 12:00:05 +0530

    Handled exceptions:

--- src/main/java/com/tms/time_management_system/controller/UserController.java
+++ src/main/java/com/tms/time_management_system/controller/UserController.java
@@ -7,11 +7,13 @@ import com.tms.time_management_system.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class UserController
 {
     @Autowired
